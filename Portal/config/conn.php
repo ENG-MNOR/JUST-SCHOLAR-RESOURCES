51e453160f901f5914fab49d37587281e710fff1
Abdulrahman-Haji <77126546+ENG-CJ@users.noreply.github.com>
2024-08-17 21:49:36 +0300

    save

--- Portal/config/conn.php
+++ Portal/config/conn.php
@@ -6,7 +6,7 @@ class DatabaseConnection{
 
     public static function getConnection(): bool | mysqli {
 
-        $conn = new mysqli("localhost","root","", "scholars");
+        $conn = new mysqli("mysql-1cfa0c07-abdulrahmandev10-95ac.e.aivencloud.com", "avnadmin", "AVNS_cizSOvTZDrnfsqP9ZdO", "just_scholars");
         if($conn->connect_error)
            return false;
 
