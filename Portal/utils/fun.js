16ddcbbb8ed5c415386841b8b79a5e051a132765
amohamud <cj@gmail.com>
2024-08-18 15:50:54 +0300

    update

--- Portal/utils/fun.js
+++ Portal/utils/fun.js
@@ -2,3 +2,16 @@ function formatDate(dbDate) {
     const datePart = dbDate.split(' ')[0];
     return datePart; 
 }
+
+function formatNumber(num) {
+    if (num >= 1000000000) {
+        return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'b';
+    }
+    if (num >= 1000000) {
+        return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'm';
+    }
+    if (num >= 1000) {
+        return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'k';
+    }
+    return num;
+}
